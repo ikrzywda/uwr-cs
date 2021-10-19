2f7526815f632489d37e013cc4e2eb0ab7c8c9cf
ikrz <ikrz@localhost.localdomain>
2021-10-19 17:22:57 +0200

    synchro

---
+++ 19-10-2021/base_conversion.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <math.h>
+
+unsigned long long int convert_to_decimal(unsigned p, char *n) {
+    char c;
+    unsigned int power;
+    unsigned long long int output = 0;
+    while ((c = *(n++)) != '\0') {
+         
+    }
+    return 0;
+}
+
+char *convert_bases(unsigned p, unsigned q, char *n) {
+    return 0;
+}
+
+
+int main() {
+
+}
