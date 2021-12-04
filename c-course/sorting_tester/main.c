1526fad205474d26c3d323c7b976c418ef39df8d
ikrz <ikrz@localhost.localdomain>
2021-12-05 00:02:10 +0100

    working testing suite for checking sorting algorithms' performance

--- c-course/sorting_tester/main.c
+++ c-course/sorting_tester/main.c
@@ -1,6 +1,24 @@
 #include "tester.h"
 
+void isort(int len, double *arr) {
+    int j, k;
+    for (int i = 1; i < len; ++i) {
+        k = arr[i];
+        j = i - 1;
+        while (j >= 0 && arr[j] > k) {
+            arr[j+1] = arr[j];
+            --j;
+        }
+        arr[j+1] = k;
+    }
+}
+
+void isort_wrapper(void *arr, size_t len, size_t size, 
+        int(*cmp)(const void*, const void*)) {
+    isort(len, arr);
+}
+
 int main() {
-    TestData *t = new_test(30, 10);
-    print_test_data(t);
+    test_sorting(qsort);
+    test_sorting(isort_wrapper);
 }
--- c-course/sorting_tester/tester.c
+++ c-course/sorting_tester/tester.c
@@ -1,44 +1,146 @@
 #include "tester.h"
 
-void print_test_data(TestData *td) {
-    printf("Input size: %d\n", td->size);
-    for (int i = 0; i < td->size; ++i) {
-        printf("%2.f ", td->input[i]);
+const unsigned kTestCount = 100;
+const unsigned kSetCount = 3;
+const unsigned kArrLengths[3] = {5000, 10000, 50000};
+
+struct test {
+    clock_t *results;
+    double *inarr;
+    double *outarr;
+    void (*sortfun)(void *arr, size_t len, size_t el_size, 
+            int (*cmp)(const void*, const void*));
+};
+
+double min_time(double *r, int test_set) {
+    int k = kTestCount * test_set;
+    double min = r[k];
+    for (int i = k + 1; i < k + kTestCount; ++i) {
+        if (r[i] < min) {
+            min = r[i];
+        }
+    }
+    return min;
+}
+
+double max_time(double *r, int test_set) {
+    int k = kTestCount * test_set;
+    double max = 0;
+    for (int i = k; i < k + kTestCount; ++i) {
+        if (r[i] > max) {
+            max = r[i];
+        }
+    }
+    return max;
+}
+
+double mean_time(double *r, int test_set) {
+    int k = kTestCount * test_set;
+    double mean = 0;
+    for (int i = k; i < k + kTestCount; ++i) {
+        mean += r[i];
+    }
+    return mean / kTestCount;
+}
+
+void print_arr(int len, double *arr) {
+    for (int i = 0; i < len; ++i) {
+        printf("%2.f ", arr[i]);
+    }
+    putchar('\n');
+}
+
+void print_results(double *r) {
+    double out_data[kSetCount][3];
+    printf("size\tmin\t\tmax\t\tmean\n");
+    for (int i = 0; i < kSetCount; ++i) {
+        printf("%d\t", kArrLengths[i]);
+        out_data[i][0] = min_time(r, i);
+        out_data[i][1] = max_time(r, i);
+        out_data[i][2] = mean_time(r, i);
+        printf("%f\t%f\t%f\n", out_data[i][0], out_data[i][1], out_data[i][2]);
     }
 }
 
-void rand_fill(int size, double *arr) {
-    for (int i = 0; i  < size; ++i) {
+int cmp(const void *a, const void *b) {
+    double ca = *(double*)a;
+    double cb = *(double*)b;
+    if (ca < cb) {
+        return -1;
+    } else if (ca > cb) {
+        return 1;
+    } 
+    return 0;
+}
+
+void throw_error(int err_code) {
+    if (err_code == NOT_PERM) {
+        printf("Error. Algorithm does not preserve elements\n");
+    } else if (err_code == NOT_SORTED) {
+        printf("Error. Algorithm does not sort in non-increasing order\n");
+    }
+    exit(1);
+}
+
+void fill_randvals(int len, double *arr) {
+    for (int i = 0; i < len; ++i) {
         arr[i] = rand();
     }
 }
 
-int is_sorted(int size, double *arr) {
-    for (int i = 1; i < size; ++i) {
-        if (arr[i-1] > arr[i]) {
+int is_perm(int len, double *a, double *b) {
+    int *el;
+    qsort(a, len, sizeof(double), cmp);
+    for (int i = 0; i < len; ++i) {
+        el = (int*)bsearch(&b[i], a, len, sizeof(double), cmp);
+        if (el == NULL) {
             return 0;
         }
     }
     return 1;
 }
 
-int is_perm(TestData *td) {
-    return 0; 
+int is_sorted(int len, double *a) {
+    for (int i = 1; i < len; ++i) {
+        if (a[i-1] > a[i]) {
+            return 0;
+        }
+    }
+    return 1;
 }
 
-TestData *new_test(int size, int test_count) {
-    TestData *new_test = malloc(sizeof(TestData));
-    new_test->size = size;
-    new_test->test_count = test_count;
-    new_test->input = malloc(sizeof(double) * size);
-    new_test->output = malloc(sizeof(double) * size);
-    new_test->times = malloc(sizeof(double) * test_count);
-    rand_fill(size, new_test->input);
-    return new_test;
+double time_sort(int len, double *inarr, double *outarr, sortfun sort) {
+    clock_t end, start = clock();
+    sort(outarr, len, sizeof(double), cmp);
+    end = clock();
+    if (!is_perm(len, inarr, outarr)) {
+        return NOT_PERM;
+    }
+    if (!is_sorted(len, outarr)) {
+        return NOT_SORTED;
+    }
+    return (double)(end - start) / CLOCKS_PER_SEC;
 }
 
-void free_test(TestData *td) {
-    free(td->input);
-    free(td->output);
-    free(td);
+
+void test_sorting(sortfun sort) {
+    int offset = 0;
+    double results[kTestCount * kSetCount];
+    double *inarr, *outarr;
+    for (int i = 0; i < kSetCount; ++i) {
+        inarr = malloc(sizeof(double) * kArrLengths[i]);
+        outarr = malloc(sizeof(double) * kArrLengths[i]);
+        for (int j = 0; j <= kTestCount; ++j) {
+            fill_randvals(kArrLengths[i], inarr);
+            memcpy(outarr, inarr, sizeof(double) * kArrLengths[i]);
+            if ((results[offset + j] = time_sort(kArrLengths[i], inarr, 
+                                                outarr, sort)) < 0) {
+                throw_error((int)results[offset + j]);
+                return;
+            }
+        }
+        free(inarr); free(outarr);
+        offset += kTestCount;
+    }
+    print_results(results);
 }
