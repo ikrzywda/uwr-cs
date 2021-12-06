254181188d59b78b4c34e1a4072c99e5599ed3aa
ikrzywda <krzywda.igor@protonmail.com>
2021-12-06 20:25:08 +0100

    sorting tester working

--- c-course/sorting_tester/main.c
+++ c-course/sorting_tester/main.c
@@ -1,24 +1,35 @@
 #include "tester.h"
+#include <string.h>
 
-void isort(int len, double *arr) {
-    int j, k;
-    for (int i = 1; i < len; ++i) {
-        k = arr[i];
-        j = i - 1;
-        while (j >= 0 && arr[j] > k) {
-            arr[j+1] = arr[j];
-            --j;
+void merge(double *arr, double *left, double *right, int n) {
+    int i = 0, li = 0, ri = 0;
+    for (; i < n; ++i) {
+        if (ri == (n/2 + n%2) || (li < n/2 && left[li] < right[ri])) {
+            arr[i] = left[li++];
+        } else {
+            arr[i] = right[ri++];
         }
-        arr[j+1] = k;
     }
 }
 
-void isort_wrapper(void *arr, size_t len, size_t size, 
-        int(*cmp)(const void*, const void*)) {
-    isort(len, arr);
+void msort(int n, double *arr) {
+    double *left, *right;
+    if (n == 1) return;
+    left = malloc((n/2) * sizeof(double)); 
+    right = malloc((n/2 + n%2) * sizeof(double));
+    memcpy(left, arr, n/2 * sizeof(double));
+    memcpy(right, arr + n/2, (n/2 + n%2) * sizeof(double));
+    msort(n/2, left);
+    msort((n/2 + n%2), right);
+    merge(arr, left, right, n);
+    free(left); free(right);
+}
+
+void msort_wrapper(void *arr, size_t len, size_t el_size, int(*cmp)(const void*, const void*)) {
+    msort((int)len, (double*)arr);
 }
 
 int main() {
     test_sorting(qsort);
-    test_sorting(isort_wrapper);
+    test_sorting(msort_wrapper);
 }
