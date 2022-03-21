b93a7f35c8a552ba37500479bd3dca9228f747c7
ikrz <igor.krzywda@improvoc.com>
2022-03-21 18:20:40 +0100

    predicates - more:

--- second_sem/programming_methodology/exercises/set-representation.rkt
+++ second_sem/programming_methodology/exercises/set-representation.rkt
@@ -7,7 +7,7 @@
   (lambda (x)
     (if (equal? a x)
       `(,a)
-      #f
+      empty-set
   )))
 
 (define (in s)
@@ -15,16 +15,17 @@
   (ormap (lambda (curr) (eq? curr el)) s)))
 
 (define (union s t)
-  (cond [(null? s) t]
-        [(in (car s) t)
-          (union (rest s) t)]
-        [else 
-          (cons (car s) (union (rest s) t))])
+  (define in-s (in s))
+  (define in-t (in t))
+  (lambda (el)
+    (or (in-s el) (in-t el))
+  )
 )
 
-(define (test t s)
-(flatten `(,s ,t)))
-
 (define (intersection s t)
-  (filter-not (lambda (curr) (and (in curr s) (or (in curr t)))) (union s t))
+  (define in-s (in s))
+  (define in-t (in t))
+  (lambda (el)
+    (and (in-s el) (in-t el))
+  )
 )
