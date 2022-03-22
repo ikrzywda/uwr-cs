f08389a72a015bceeb39292e722d840b13400094
ikrz <igor.krzywda@improvoc.com>
2022-03-22 09:05:32 +0100

    cleanup

--- second_sem/programming_methodology/exercises/is-sorted.rkt
+++
@@ -1,5 +0,0 @@
-#lang racket
-
-(define (is-sorted lst)
-    (andmap > (append lst (list +inf.0)) (append (list -inf.0) lst))
-)
--- second_sem/programming_methodology/exercises/my-if.rkt
+++
@@ -1,5 +0,0 @@
-#lang racket
-
-; not lazy
-(define (my-if if then else)
-    (and (or if else) then))
